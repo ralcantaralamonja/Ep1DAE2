d682136409e61582f423d8f22ffd6f57b6ec3fa0
Ricardo Alcantara <alcantararicardo04@gmail.com>
2023-10-08 13:36:45 -0500

    Agregando validaciones por backend

--- src/main/java/com/isil/saturno_1431/model/Estudiante.java
+++ src/main/java/com/isil/saturno_1431/model/Estudiante.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import org.springframework.data.annotation.Reference;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 
 @Entity
@@ -13,12 +16,20 @@ public class Estudiante {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer Id;
-
+    @NotBlank
+    @NotNull
     private String nombres;
+    @NotBlank
+    @NotNull
     private String apellidos;
     @Column(unique = true)
+    @Size(max = 8, min = 8)
     private  Integer dni;
+    @NotNull
+    @NotBlank
     private  String correo;
+
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "id_carrera") // Especifica el nombre de la columna de la clave foránea en la tabla Estudiante
     private Carrera carrera;
