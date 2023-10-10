a4950651771da8de724b26e2ece91a2d65536e28
Ricardo Alcantara <alcantararicardo04@gmail.com>
2023-10-10 01:02:43 -0500

    Ingresar validado

--- src/main/java/com/isil/saturno_1431/controller/EstudianteControler.java
+++ src/main/java/com/isil/saturno_1431/controller/EstudianteControler.java
@@ -1,17 +1,17 @@
 package com.isil.saturno_1431.controller;
 
+
 import com.isil.saturno_1431.exception.EstudianteNotFundException;
 import com.isil.saturno_1431.model.Estudiante;
 import com.isil.saturno_1431.repository.EstudianteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,43 +19,40 @@ import java.util.Optional;
 @RequestMapping("/estudiantes")
 public class EstudianteControler {
 
-
     @Autowired
     private EstudianteRepository estudianteRepository;
 
     @GetMapping("")
-    String estudianteindex (Model model){
+    public String estudianteIndex(Model model) {
         List<Estudiante> estudiantes = estudianteRepository.findAll();
         model.addAttribute("estudiantes", estudiantes);
         return "estudiantesindex";
+    }
 
+    @GetMapping("/nuevo")
+    public String nuevoEstudianteForm(Model model) {
+        model.addAttribute("estudiante", new Estudiante());
+        return "ingresarEstudiante";
     }
 
     @PostMapping("/crear")
-    public String crear(RedirectAttributes redirectAttributes, @ModelAttribute Estudiante estudiante, BindingResult bindingResult) {
+    public String crearEstudiante(
+            RedirectAttributes redirectAttributes,
+            @Valid @ModelAttribute Estudiante estudiante,
+            BindingResult bindingResult
+    ) {
+        if (bindingResult.hasErrors()) {
+            return "ingresarEstudiante";
+        }
 
         try {
             estudianteRepository.save(estudiante);
-            redirectAttributes.addFlashAttribute("exito", true); // Agregar mensaje de éxito
+            redirectAttributes.addFlashAttribute("exito", true);
         } catch (Exception e) {
-            redirectAttributes.addFlashAttribute("error", true); // Agregar mensaje de error
-            e.printStackTrace(); // Puedes registrar la excepción si es necesario
+            redirectAttributes.addFlashAttribute("error", true);
+            e.printStackTrace();
         }
         return "redirect:/estudiantes";
-    }
-
-
-
-
-    @DeleteMapping("/eliminar/{id}")
-    public String eliminarEstudiante(@PathVariable int id) {
-        Optional<Estudiante> optionalEstudiante = estudianteRepository.findById(id);
-        if (optionalEstudiante.isPresent()) {
-            estudianteRepository.delete(optionalEstudiante.get());
-            return "redirect:/estudiantes"; // Redirige después de eliminar
-        } else {
-            throw new EstudianteNotFundException(id);
-        }
     }
 
     @GetMapping("/editar/{id}")
@@ -65,41 +62,48 @@ public class EstudianteControler {
         if (optionalEstudiante.isPresent()) {
             Estudiante estudiante = optionalEstudiante.get();
             model.addAttribute("estudiante", estudiante);
-            return "editarEstudiante"; // Reemplaza "editarEstudiante" con el nombre de tu vista de edición
+            return "editarEstudiante";
         } else {
             throw new EstudianteNotFundException(id);
         }
     }
+
     @PostMapping("/editar/{id}")
-    public String actualizarEstudiante(Model model, @ModelAttribute @Validated Estudiante estudiante, BindingResult bindingResult) {
-        if(bindingResult.hasErrors()){
-            model.addAttribute("estudiante", estudiante);
+    public String actualizarEstudiante(
+            Model model,
+            @PathVariable int id,
+            @Valid @ModelAttribute Estudiante estudiante,
+            BindingResult bindingResult
+    ) {
+        if (bindingResult.hasErrors()) {
             return "editarEstudiante";
         }
-        // Verifica si el estudiante existe en la base de datos
-        Optional<Estudiante> optionalEstudiante = estudianteRepository.findById(estudiante.getId());
+
+        Optional<Estudiante> optionalEstudiante = estudianteRepository.findById(id);
+
         if (optionalEstudiante.isPresent()) {
-            // Actualiza los datos del estudiante con los nuevos valores
             Estudiante existingEstudiante = optionalEstudiante.get();
-            existingEstudiante.setId(estudiante.getId());
             existingEstudiante.setNombres(estudiante.getNombres());
             existingEstudiante.setApellidos(estudiante.getApellidos());
             existingEstudiante.setDni(estudiante.getDni());
             existingEstudiante.setCorreo(estudiante.getCorreo());
             existingEstudiante.setCarrera(estudiante.getCarrera());
 
-            // Guarda el estudiante actualizado en la base de datos
             estudianteRepository.save(existingEstudiante);
-            return "redirect:/estudiantes"; // Redirige después de actualizar
+            return "redirect:/estudiantes";
         } else {
-            throw new EstudianteNotFundException(estudiante.getId());
+            throw new EstudianteNotFundException(id);
         }
     }
 
-
-
+    @DeleteMapping("/eliminar/{id}")
+    public String eliminarEstudiante(@PathVariable int id) {
+        Optional<Estudiante> optionalEstudiante = estudianteRepository.findById(id);
+        if (optionalEstudiante.isPresent()) {
+            estudianteRepository.delete(optionalEstudiante.get());
+            return "redirect:/estudiantes";
+        } else {
+            throw new EstudianteNotFundException(id);
+        }
+    }
 }
-
-
-
-
